45a873108058ff121b52383d8d60e5aeb653ace2
qwert <qwert@gmail.com>
2025-11-30 02:46:14 +0500

    celery 1.0

--- api/celery.py
+++ api/celery.py
@@ -1,5 +1,6 @@
 from celery import Celery
-
+import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'yondosh_core.settings')
 app = Celery('tasks')
 app.config_from_object("django.conf:settings", namespace="CELERY")
 app.autodiscover_tasks()
